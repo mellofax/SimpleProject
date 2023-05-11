f96e58040b1fbe40eca1dd299abf83b7a5229f5f
Nikita Bialko <barmansuperman@gmail.com>
2023-05-11 13:34:34 +0300

    commit

--- Tests/simpleTests.py
+++ Tests/simpleTests.py
@@ -12,3 +12,12 @@ class firstTests:
         get_started.click()
         time.sleep(4)
         expect(page).to_have_url(re.compile(".*intro"))
+
+    def test_second(self, page):
+        page.goto(configurePlaywright.URL)
+        expect(page).to_have_title(re.compile("Playwright"))
+        get_started = page.get_by_role("link", name="Get started")
+        expect(get_started).to_have_attribute("href", "/docs/intro")
+        get_started.click()
+        time.sleep(4)
+        expect(page).to_have_url(re.compile(".*intro"))

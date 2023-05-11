f1e429e8f1f34eac7a5c12bac2c43e8b96c211fa
Nikita Bialko <barmansuperman@gmail.com>
2023-05-11 11:43:34 +0300

    Initial commit

---
+++ Tests/simpleTests.py
@@ -0,0 +1,10 @@
+class firstTests:
+    def test_first(self):
+        num1 = 4
+        num2 = 24
+        assert num1 < num2
+
+    def test_second(self):
+        num1 = 4
+        num2 = 4
+        assert num1 == num2
